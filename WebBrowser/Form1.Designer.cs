7d365bffd301aec70fbeb12428c12ea7e33a3868
elmen <elmen@DESKTOP-Q04NIBN>
2022-07-09 22:46:13 -0400

    Add project files.

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnback = new System.Windows.Forms.ToolStripButton();
+            this.btnforward = new System.Windows.Forms.ToolStripButton();
+            this.btnrefresh = new System.Windows.Forms.ToolStripButton();
+            this.btnaddtab = new System.Windows.Forms.ToolStripButton();
+            this.btndeltab = new System.Windows.Forms.ToolStripButton();
+            this.txturl = new System.Windows.Forms.ToolStripTextBox();
+            this.btnsearch = new System.Windows.Forms.ToolStripButton();
+            this.btnhome = new System.Windows.Forms.ToolStripButton();
+            this.btnfav = new System.Windows.Forms.ToolStripButton();
+            this.cmbfav = new System.Windows.Forms.ToolStripComboBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btndelfav = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.AllowItemReorder = true;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnback,
+            this.btnforward,
+            this.btnrefresh,
+            this.btnaddtab,
+            this.btndeltab,
+            this.txturl,
+            this.btnsearch,
+            this.btnhome,
+            this.btnfav,
+            this.btndelfav,
+            this.cmbfav});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1047, 27);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnback
+            // 
+            this.btnback.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnback.Image = ((System.Drawing.Image)(resources.GetObject("btnback.Image")));
+            this.btnback.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnback.Name = "btnback";
+            this.btnback.Size = new System.Drawing.Size(23, 24);
+            this.btnback.Text = "toolStripButton1";
+            this.btnback.ToolTipText = "Back";
+            this.btnback.Click += new System.EventHandler(this.btnback_Click);
+            // 
+            // btnforward
+            // 
+            this.btnforward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnforward.Image = ((System.Drawing.Image)(resources.GetObject("btnforward.Image")));
+            this.btnforward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnforward.Name = "btnforward";
+            this.btnforward.Size = new System.Drawing.Size(23, 24);
+            this.btnforward.Text = "toolStripButton2";
+            this.btnforward.ToolTipText = "Forward";
+            this.btnforward.Click += new System.EventHandler(this.btnforward_Click);
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnrefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnrefresh.Image")));
+            this.btnrefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(23, 24);
+            this.btnrefresh.Text = "toolStripButton3";
+            this.btnrefresh.ToolTipText = "Refresh";
+            // 
+            // btnaddtab
+            // 
+            this.btnaddtab.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnaddtab.Image = ((System.Drawing.Image)(resources.GetObject("btnaddtab.Image")));
+            this.btnaddtab.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnaddtab.Name = "btnaddtab";
+            this.btnaddtab.Size = new System.Drawing.Size(23, 24);
+            this.btnaddtab.Text = "New Tab";
+            this.btnaddtab.Click += new System.EventHandler(this.btnaddtab_Click);
+            // 
+            // btndeltab
+            // 
+            this.btndeltab.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btndeltab.Image = ((System.Drawing.Image)(resources.GetObject("btndeltab.Image")));
+            this.btndeltab.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btndeltab.Name = "btndeltab";
+            this.btndeltab.Size = new System.Drawing.Size(23, 24);
+            this.btndeltab.Text = "Del Tab";
+            this.btndeltab.Click += new System.EventHandler(this.btndeltab_Click);
+            // 
+            // txturl
+            // 
+            this.txturl.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(500, 27);
+            this.txturl.Enter += new System.EventHandler(this.txturl_Enter);
+            this.txturl.Click += new System.EventHandler(this.txturl_Click);
+            this.txturl.DoubleClick += new System.EventHandler(this.txturl_DoubleClick);
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnsearch.Image = ((System.Drawing.Image)(resources.GetObject("btnsearch.Image")));
+            this.btnsearch.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(23, 24);
+            this.btnsearch.Text = "toolStripButton1";
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // btnhome
+            // 
+            this.btnhome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnhome.Image = ((System.Drawing.Image)(resources.GetObject("btnhome.Image")));
+            this.btnhome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnhome.Name = "btnhome";
+            this.btnhome.Size = new System.Drawing.Size(23, 24);
+            this.btnhome.Text = "toolStripButton4";
+            this.btnhome.Click += new System.EventHandler(this.btnhome_Click);
+            // 
+            // btnfav
+            // 
+            this.btnfav.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnfav.Image = ((System.Drawing.Image)(resources.GetObject("btnfav.Image")));
+            this.btnfav.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnfav.Name = "btnfav";
+            this.btnfav.Size = new System.Drawing.Size(23, 24);
+            this.btnfav.Text = "Add Current Page to Favorites";
+            this.btnfav.Click += new System.EventHandler(this.btnfav_Click);
+            // 
+            // cmbfav
+            // 
+            this.cmbfav.AutoToolTip = true;
+            this.cmbfav.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbfav.Name = "cmbfav";
+            this.cmbfav.Size = new System.Drawing.Size(370, 23);
+            this.cmbfav.ToolTipText = "Lista de Favoritos";
+            this.cmbfav.Click += new System.EventHandler(this.cmbfav_Click);
+            this.cmbfav.TextChanged += new System.EventHandler(this.cmbfav_TextChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1047, 384);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1039, 358);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "New Tab";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1047, 384);
+            this.panel1.TabIndex = 5;
+            // 
+            // btndelfav
+            // 
+            this.btndelfav.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btndelfav.Image = ((System.Drawing.Image)(resources.GetObject("btndelfav.Image")));
+            this.btndelfav.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btndelfav.Name = "btndelfav";
+            this.btndelfav.Size = new System.Drawing.Size(23, 24);
+            this.btndelfav.Text = "Delete From Favorite";
+            this.btndelfav.Click += new System.EventHandler(this.btndelfav_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1047, 411);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Web Browser Buret";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnback;
+        private System.Windows.Forms.ToolStripButton btnforward;
+        private System.Windows.Forms.ToolStripButton btnrefresh;
+        private System.Windows.Forms.ToolStripTextBox txturl;
+        private System.Windows.Forms.ToolStripButton btnsearch;
+        private System.Windows.Forms.ToolStripButton btnhome;
+        private System.Windows.Forms.ToolStripButton btnfav;
+        private System.Windows.Forms.ToolStripComboBox cmbfav;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ToolStripButton btnaddtab;
+        private System.Windows.Forms.ToolStripButton btndeltab;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripButton btndelfav;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using CefSharp.WinForms.Internals;
+using CefSharp.Web;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        int index = 0;
+        readonly string home = "www.Google.com";
+        List<ChromiumWebBrowser> browsers = new List<ChromiumWebBrowser>();
+        List<string> favs = new List<string>();
+        string file = "Favoritos.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            browsers.Add(new ChromiumWebBrowser(home));
+            browsers[0].Dock = DockStyle.Fill;
+            tabPage1.Controls.Add(browsers[0]);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txturl.Text = home;
+            btnhome_Click(sender, e);
+            favs.Clear();
+            if (File.Exists(file))
+            {
+                favs.AddRange(File.ReadAllLines(file));
+                foreach (var fav in favs)
+                {
+                    cmbfav.Items.Add(fav);
+                }
+                
+            }
+            else
+            {
+                File.Create(file);
+            }
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            if (txturl.Text.Trim().Length!=0)
+            {
+                browsers[index].Load(txturl.Text);
+                txturl.Text = browsers[index].Address;
+            }
+        }
+
+        private void btnhome_Click(object sender, EventArgs e)
+        {
+            browsers[index].LoadUrlAsync(home);
+        }
+
+        private void btnback_Click(object sender, EventArgs e)
+        {
+            if (browsers[index].CanGoBack)
+            {
+                browsers[index].Back();
+            }
+        }
+
+        private void btnforward_Click(object sender, EventArgs e)
+        {
+            if (browsers[index].CanGoForward)
+            {
+                browsers[index].Forward();
+            }
+        }
+
+        private void Save_Favs()
+        {
+            foreach (var item in cmbfav.Items)
+            {
+                favs.Add(item.ToString());
+            }
+            File.WriteAllLines(file, favs);
+            favs.Clear();
+        }
+        private void btnfav_Click(object sender, EventArgs e)
+        {
+            if (txturl.Text.Trim().Length!=0)
+            {
+                cmbfav.Items.Add(txturl.Text);
+                Save_Favs();
+                MessageBox.Show("Página añadida a favoritos.");
+            }
+            
+        }
+
+        private void btndelfav_Click(object sender, EventArgs e)
+        {
+            if (cmbfav.Text.Trim().Length != 0)
+            {
+                cmbfav.Items.Remove(txturl.Text);
+                Save_Favs();
+                MessageBox.Show("Página eliminada de favoritos.");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+        }
+
+       
+        private void txturl_Click(object sender, EventArgs e)
+        {
+            txturl.Text = browsers[index].Address;
+        }
+
+
+        private void txturl_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txturl_DoubleClick(object sender, EventArgs e)
+        {
+            txturl.SelectAll();
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex != -1)
+            {
+                index = tabControl1.SelectedIndex;
+                txturl.Text = browsers[index].Address;
+            }
+            
+        }
+
+        private void btnaddtab_Click(object sender, EventArgs e)
+        {   
+            index++;
+            TabPage tab = new TabPage();
+            tab.Text = "New Tab";
+            browsers.Add(new ChromiumWebBrowser(home));
+            browsers[index].Dock = DockStyle.Fill;
+            tab.Controls.Add(browsers[index]);
+            tabControl1.TabPages.Add(tab);
+            tabControl1.SelectTab(index);
+        }
+
+        private void btndeltab_Click(object sender, EventArgs e)
+        {
+            browsers.RemoveAt(index);
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+        }
+
+        private void cmbfav_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbfav_TextChanged(object sender, EventArgs e)
+        {
+            cmbfav.Text = txturl.Text;
+            browsers[index].Load(txturl.Text);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            int x = 0;
+            foreach (TabPage tab in tabControl1.TabPages)
+            {
+                tabControl1.TabPages.Remove(tab);
+                browsers[x].Hide();
+                x++;
+            }
+        }
+
+        private void btndelfav_Click_1(object sender, EventArgs e)
+        {
+            if (cmbfav.Text.Trim().Length != 0)
+            {
+                cmbfav.Items.Remove(txturl.Text);
+                favs.Remove(txturl.Text);
+                Save_Favs();
+                MessageBox.Show("Página eliminada de favoritos.");
+            }
+        }
+    }
+}
